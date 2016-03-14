992196b9e5055ec56ed3a246f8bd26366b3fc484
Luis Lopez <luigibyte@gmail.com>
2016-03-14 12:06:19 -0300

    show position on map

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,10 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "pluginId": "plugin.google.maps",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
-    "cordova-plugin-geolocation": "2.1.1-dev"
+    "cordova-plugin-geolocation": "2.1.1-dev",
+    "plugin.google.maps": "1.3.9"
 }
 // BOTTOM OF METADATA
 });
